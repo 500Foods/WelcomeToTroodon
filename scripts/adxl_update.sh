6e31b74890702cd35e928a641ca936a35ae8944b
Andrew Simard <41052272+500Foods@users.noreply.github.com>
2024-05-09 22:36:04 -0700

    Create adxl_update.sh

---
+++ scripts/adxl_update.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/bash
+
+sudo service klipper stop
+cd ~/klipper
+git pull
+make clean
+make menuconfig
+make
+make flash FLASH_DEVICE=/dev/serial/by-id/usb-1a86_USB2.0-Serial-if00-port0
+sudo service klipper start
+
+In Menuconfig:
+    Microcontroller: Raspberry Pi RP 2040
+    Com Interface: USBSERIAL
