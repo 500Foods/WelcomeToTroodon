1ea6cfc403fddd1708213eb088789f111aada9a7
Andrew Simard <41052272+500Foods@users.noreply.github.com>
2024-05-27 20:04:49 -0700

    Update get_current_datetime.sh

--- scripts/get_current_datetime.sh
+++ scripts/get_current_datetime.sh
@@ -6,4 +6,4 @@
 # This is intended to be run from Klipper via the gcode_shell_command
 # extension, which can be installed using KIAUH.
 
-echo "Current Time:" `date +"%Y-%m-%dT%H:%M:%S%z"`
+echo "Current Date/Time:" `date +"%Y-%m-%dT%H:%M:%S%z"`
